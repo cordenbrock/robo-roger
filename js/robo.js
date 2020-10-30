2e60ca0281a884388fab07365bf3f7a47b6d38d2
cordenbrock <cordenbrock@gmail.com>
2020-10-30 12:05:07 -0700

    generate/populate array from input, robofy array, and return robo output

--- js/robo.js
+++ js/robo.js
@@ -1,28 +1,48 @@
 // collects input and populates an array from 0 to userNumber  
-function populateUserArray(num) {
+function populateRoboArray(num) {
     let numArray = [];
     for (let i = 0; i <= num; i++) {
       numArray.push(i);
     }
     return numArray;
-  };
+};
 
+function robofyArray(num) {
+    num = num.toString();
+    console.log(num)
+    if (num.includes(3)) {
+        return "neighbor!!!";
+      } else if (num.includes(2)) {
+        return "boop!!";
+      } else if (num.includes(1)) {
+        return "beep!";
+      } else {
+        return num;
+      };
+}
 
   // DOM -- UI
 $(document).ready(() => {
     // form submission
     $('#form').submit(e => {
         e.preventDefault();
+        
         // collect user input
-        let userNumber, roboArray, result;
+        let userNumber, roboArray, roboResult;
         userNumber = parseInt($('#number').val());
-        console.log(userNumber)
 
-        // generate an array, modify elements by certain rules, convert to displayable data type
+        // generate robo array
+        roboArray = populateRoboArray(userNumber);
+        console.log(roboArray);
+
+        // iterate through robo array and return new array with robofied elements
+        roboResult = roboArray.map(num => {
+            return robofyArray(num);
+          });
 
         // display result || invalidate input
         if (userNumber > 0 && userNumber < 100000) {
-            $("#print-result").text(userNumber);
+            $("#output").text(roboResult);
         } else {
             alert("invalid input")
         }
