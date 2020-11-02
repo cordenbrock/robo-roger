93379c5aabffccbeb0b738b45ed7c906efc50fc7
cordenbrock <cordenbrock@gmail.com>
2020-11-01 16:50:31 -0800

    fix indentation

--- js/robo.js
+++ js/robo.js
@@ -1,43 +1,41 @@
-// Controller -- Business logic
+// Business logic
 
 function populateUserArray(num) {
-    let numArray = [];
-    for (let i = 0; i <= num; i++) {
-      numArray.push(i);
-    }
-    return numArray;
+  let numArray = [];
+  for (let i = 0; i <= num; i++) {
+    numArray.push(i)
+  };
+  return numArray;
 };
 function robofyArray(num) {
-    num = num.toString();
-    if (num.includes(3)) {
-        return "neighbor!!!";
-      } else if (num.includes(2)) {
-        return "boop!!";
-      } else if (num.includes(1)) {
-        return "beep!";
-      } else {
-        return num;
-      };
+  num = num.toString();
+  if (num.includes(3)) {
+      return "neighbor!!!";
+    } else if (num.includes(2)) {
+      return "boop!!";
+    } else if (num.includes(1)) {
+      return "beep!";
+    } else {
+      return num;
+    };
 }
 
-// DOM -- UI logic
+// UI logic
 $(document).ready(() => {
 
-    $('#form').submit(e => {
-        e.preventDefault();
-   
-        let userNumber, userArray, robofiedArray, roboResult;
-        userNumber = parseInt($('#number').val());
-        userArray = populateUserArray(userNumber);
-        robofiedArray = userArray.map(num => {
-            return robofyArray(num);
-          });
-        roboResult = robofiedArray.join(' ')
-        if (userNumber > 0) {
-            $("#output").text(roboResult);
-            $("#output").show();
-        } else {
-            alert("invalid un-neighborly input")
-        }
-    })
+  $('#form').submit(e => {
+    e.preventDefault();
+
+    let userNumber, userArray, robofiedArray, roboResult;
+    userNumber = parseInt($('#number').val());
+    userArray = populateUserArray(userNumber);
+    robofiedArray = userArray.map(num => robofyArray(num));
+    roboResult = robofiedArray.join(' ')
+    if (userNumber > 0) {
+      $("#output").text(roboResult);
+      $("#output").show();
+    } else {
+      alert("invalid: un-neighborly input")
+    }
+  })
 });
